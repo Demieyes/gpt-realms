a33edb1e8b1867abe204eea621bc2df80ff7f57d
Demieyes <dylansbeginning@gmail.com>
2026-09-08 21:50:10 -0400

    Load equipment attachment fix v0.3.3

--- public/launcher.js
+++ public/launcher.js
@@ -49,7 +49,7 @@
     enter.textContent='Entering…';
     $('boot-status').textContent='Building Aestra…';
     try{
-      const mod=await import('/game-bundle.js?v=032');
+      const mod=await import('/game-bundle.js?v=033');
       await mod.startGame(selected);
       $('launcher').classList.add('hidden');
       $('hud').classList.remove('hidden');
